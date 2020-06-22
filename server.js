206d05abf43aa0395110f314f887ccfe2d1479db
Anish <anish.vahora5@gmail.com>
2020-06-22 11:03:46 +0530

    server side router changes

--- server.js
+++ server.js
@@ -23,14 +23,14 @@ app.use('/api/node-tree',NodeTree);
 app.use('/api/upload',upload);
 
 // Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
+
     // Set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, 'client/build')));
   
-    app.get('/*', (req, res) => {
+    app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
-  }
+  
 
 
 const PORT = process.env.PORT || 5000;
