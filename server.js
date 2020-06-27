37dbc1e7c07c9d984f0a01238d88acf98608d26f
Anish <anish.vahora5@gmail.com>
2020-06-27 10:15:30 +0530

    require fs

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
+const fs = require('fs');
 const cors = require('cors');
 require('dotenv').config();
 
