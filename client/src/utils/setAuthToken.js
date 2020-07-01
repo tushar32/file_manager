7e3b554fb46b14ef327ceaf2cdd7e507db855fa4
Anish <anish.vahora5@gmail.com>
2020-07-01 10:09:24 +0530

    set https agent

--- client/src/utils/setAuthToken.js
+++ client/src/utils/setAuthToken.js
@@ -2,11 +2,13 @@ import axios from 'axios';
 import https from 'https';
 
  const setAuthToken = token => {
+    const agent = new https.Agent({  
+        rejectUnauthorized: false
+      });
+    axios.defaults.httpsAgent = agent;
+    
     if(token){
-        const agent = new https.Agent({  
-            rejectUnauthorized: false
-          });
-        axios.defaults.httpsAgent = agent;
+        
         axios.defaults.headers.common['x-auth-token'] = token;
         axios.defaults.headers.common['Accept'] = 'application/json';
 
